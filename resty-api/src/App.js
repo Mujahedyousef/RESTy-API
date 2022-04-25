f686fb3d0859e9cde628345df432aa0b0f1d8d45
Mujahedyousef <mujahedyousef97@gmail.com>
2022-04-25 23:43:27 +0300

    1

---
+++ resty-api/src/App.js
@@ -0,0 +1,14 @@
+import Header from './components/Header/header';
+import Footer from './components/Footer/footer';
+import './App.css';
+
+function App() {
+  return (
+   <div id="app">
+<Header />
+<Footer/>
+   </div>
+  );
+}
+
+export default App;
