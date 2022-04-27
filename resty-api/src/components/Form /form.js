2fd0d92c1d1eed10f648775a1ec9a75fc656efad
Mujahedyousef <mujahedyousef97@gmail.com>
2022-04-27 22:11:50 +0300

    complete phase_1

--- resty-api/src/components/Form /form.js
+++ resty-api/src/components/Form /form.js
@@ -1,11 +1,12 @@
 import './form.css';
-export default function Form() {
+export default function Form(props) {
+
     return (
         <div className="allForm" >
             <form className="form" >
 
                 <input id="urlInput" type="text" name="URL" placeholder='http://api.url.here..............' />
-                <input id="go" type="submit" value="Go" />
+                <button id="go" type="submit" value="go" onClick={props.onSubmit} >GO</button>
                 <div className="btn">
                     <button id="get" type="submit" value="Get" >Get</button>
                     <button id="Post" type="submit" value="Post" >Post</button>
